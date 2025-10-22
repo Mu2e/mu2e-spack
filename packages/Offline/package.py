54fd3056af977871db43cd7b8e9e67d6be03a9e7
Eric Flumerfelt <eflumerf@fnal.gov>
2025-10-22 08:33:52 -0500

    Update KinKal tag

--- packages/Offline/package.py
+++ packages/Offline/package.py
@@ -65,8 +65,8 @@ class Offline(CMakePackage):
     depends_on("KinKal@3.1.3", when="@11.03.00")
     depends_on("KinKal@3.1.4", when="@11.04.00")
     depends_on("KinKal@3.1.5", when="@11.05.01")
-    depends_on("KinKal@3.4.2,develop", when="@develop") # UPDATE AS NEEDED
-    depends_on("KinKal@3.4.2,main", when="@main") # UPDATE AS NEEDED
+    depends_on("KinKal@3.4.3,develop", when="@develop") # UPDATE AS NEEDED
+    depends_on("KinKal@3.4.3,main", when="@main") # UPDATE AS NEEDED
 
     depends_on("btrk")
     depends_on("gallery")
