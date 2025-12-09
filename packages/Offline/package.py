238eb3d5ab0cb4543a1971d44a8fd487f3fd950e
Eric Flumerfelt <eflumerf@fnal.gov>
2025-12-09 08:40:22 -0600

    Offline@12.05.00 depends on kinkal@3.2.1

--- packages/Offline/package.py
+++ packages/Offline/package.py
@@ -65,6 +65,7 @@ class Offline(CMakePackage):
     depends_on("kinkal@3.1.3", when="@11.03.00")
     depends_on("kinkal@3.1.4", when="@11.04.00")
     depends_on("kinkal@3.1.5", when="@11.05.01")
+    depends_on("kinkal@3.2.1", when="@12.05.00")
     depends_on("kinkal@3.5.0,develop", when="@develop") # UPDATE AS NEEDED
     depends_on("kinkal@3.5.0,main", when="@main") # UPDATE AS NEEDED
 
