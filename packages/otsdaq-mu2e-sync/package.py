2bc76d5aae8928735f582997d94737d75ff674ff
Eric Flumerfelt <eflumerf@fnal.gov>
2026-02-23 09:07:48 -0600

    Add recipe for otsdaq-mu2e-sync

---
+++ packages/otsdaq-mu2e-sync/package.py
@@ -0,0 +1,76 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+import os
+
+
+def sanitize_environments(env, *vars):
+    for var in vars:
+        env.prune_duplicate_paths(var)
+        env.deprioritize_system_paths(var)
+
+
+class OtsdaqMu2eSync(CMakePackage):
+    """FIXME: Put a proper description of your package here."""
+
+    homepage = "https://mu2e.fnal.gov"
+    git = "https://github.com/Mu2e/otsdaq-mu2e-sync.git"
+    url = (
+        "https://github.com/Mu2e/otsdaq-mu2e-sync/archive/refs/tags/v1_00_00.tar.gz"
+    )
+
+    maintainers("eflumerf", "rrivera747")
+
+    license("BSD")
+
+    version("develop", branch="develop", get_full_repo=True)
+
+    version("v1_00_00", commit="9df3faf3cc79e0b239a8ecff75a3bf69cc800dbb")
+
+    def url_for_version(self, version):
+        url = "https://github.com/Mu2e/otsdaq-mu2e-trigger/archive/refs/tags/{0}.tar.gz"
+        return url.format(version)
+
+    variant(
+        "cxxstd",
+        default="20",
+        values=("14", "17", "20"),
+        multi=False,
+        description="Use the specified C++ standard when building.",
+    )
+
+    depends_on("otsdaq-mu2e@v4_00_00:,develop")
+    depends_on("otsdaq-suite")
+    depends_on("Offline@12.00.00:,develop,main")
+    depends_on("cetmodules@3.26.00:", type="build")
+
+    def cmake_args(self):
+        args = [
+            self.define_from_variant("CMAKE_CXX_STANDARD", "cxxstd"),
+        ]
+        if os.path.exists("CMakePresets.cmake"):
+            args.extend(["--preset", "default"])
+        else:
+            self.define("artdaq_core_OLD_STYLE_CONFIG_VARS", True)
+        return args
+
+    def setup_run_environment(self, env):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
+
+    def setup_dependent_run_environment(self, env, dependent_spec):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
